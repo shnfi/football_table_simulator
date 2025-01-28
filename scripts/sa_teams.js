513c787606e721f42fa34e23567527875d117891
shnfi <mrplom7@gmail.com>
2025-01-28 12:13:22 +0330

    added this leagues's teams json file

---
+++ scripts/sa_teams.js
@@ -0,0 +1,102 @@
+window.sa_teams = [
+    {
+        "name": "AC Milan",
+        "badge": "https://crests.football-data.org/98.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "ACF Fiorentina",
+        "badge": "https://crests.football-data.org/99.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "AS Roma",
+        "badge": "https://crests.football-data.org/100.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Atalanta BC",
+        "badge": "https://crests.football-data.org/102.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Bologna FC 1909",
+        "badge": "https://crests.football-data.org/103.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Cagliari Calcio",
+        "badge": "https://crests.football-data.org/104.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Genoa CFC",
+        "badge": "https://crests.football-data.org/107.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "FC Internazionale Milano",
+        "badge": "https://crests.football-data.org/108.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Juventus FC",
+        "badge": "https://crests.football-data.org/109.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "SS Lazio",
+        "badge": "https://crests.football-data.org/110.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Parma Calcio 1913",
+        "badge": "https://crests.football-data.org/112.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "SSC Napoli",
+        "badge": "https://crests.football-data.org/113.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Udinese Calcio",
+        "badge": "https://crests.football-data.org/115.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Empoli FC",
+        "badge": "https://crests.football-data.org/445.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Hellas Verona FC",
+        "badge": "https://crests.football-data.org/450.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Venezia FC",
+        "badge": "https://crests.football-data.org/454.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Torino FC",
+        "badge": "https://crests.football-data.org/586.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "US Lecce",
+        "badge": "https://crests.football-data.org/5890.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "AC Monza",
+        "badge": "https://crests.football-data.org/5911.png",
+        "power": 10 * 10, // out of 100
+    },
+    {
+        "name": "Como 1907",
+        "badge": "https://crests.football-data.org/7397.png",
+        "power": 10 * 10, // out of 100
+    },
+]
