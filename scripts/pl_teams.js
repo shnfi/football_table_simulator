f18c91b61f2bb34ebe1f4071c4613e6feca7938e
shnfi <mrplom7@gmail.com>
2025-01-29 15:56:03 +0330

    added the power for each team

--- scripts/pl_teams.js
+++ scripts/pl_teams.js
@@ -2,32 +2,32 @@ window.pl_teams = [
     {
         "name": "Arsenal FC",
         "badge": "https://crests.football-data.org/57.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 7, // out of 100
     },
     {
         "name": "Aston Villa FC",
         "badge": "https://crests.football-data.org/58.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 5, // out of 100
     },
     {
         "name": "Chelsea FC",
         "badge": "https://crests.football-data.org/61.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 7, // out of 100
     },
     {
         "name": "Everton FC",
         "badge": "https://crests.football-data.org/62.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 6, // out of 100
     },
     {
         "name": "Fulham FC",
         "badge": "https://crests.football-data.org/63.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 4, // out of 100
     },
     {
         "name": "Liverpool FC",
         "badge": "https://crests.football-data.org/64.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 9, // out of 100
     },
     {
         "name": "Manchester City FC",
@@ -37,66 +37,66 @@ window.pl_teams = [
     {
         "name": "Manchester United FC",
         "badge": "https://crests.football-data.org/66.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 4, // out of 100
     },
     {
         "name": "Newcastle United FC",
         "badge": "https://crests.football-data.org/67.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 4, // out of 100
     },
     {
         "name": "Tottenham Hotspur FC",
         "badge": "https://crests.football-data.org/73.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 5, // out of 100
     },
     {
         "name": "Wolverhampton Wanderers FC",
         "badge": "https://crests.football-data.org/76.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 2, // out of 100
     },
     {
         "name": "Leicester City FC",
         "badge": "https://crests.football-data.org/338.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 3, // out of 100
     },
     {
         "name": "Southampton FC",
         "badge": "https://crests.football-data.org/340.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 1, // out of 100
     },
     {
         "name": "Ipswich Town FC",
         "badge": "https://crests.football-data.org/349.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 1, // out of 100
     },
     {
         "name": "Nottingham Forest FC",
         "badge": "https://crests.football-data.org/351.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 7, // out of 100
     },
     {
         "name": "Crystal Palace FC",
         "badge": "https://crests.football-data.org/354.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 6, // out of 100
     },
     {
         "name": "Brighton FC",
         "badge": "https://crests.football-data.org/397.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 3, // out of 100
     },
     {
         "name": "Brentford FC",
         "badge": "https://crests.football-data.org/402.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 3, // out of 100
     },
     {
         "name": "West Ham United FC",
         "badge": "https://crests.football-data.org/563.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 4, // out of 100
     },
     {
         "name": "AFC Bournemouth",
         "badge": "https://crests.football-data.org/bournemouth.png",
-        "power": 10 * 10, // out of 100
+        "power": 10 * 4, // out of 100
     },
 ]
